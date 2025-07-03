73a7dd41fdbe4a5087a563f0b1f81b02ff8b2e02
EAS-Tyler <tyler@enterpriseautomation.co.uk>
2025-07-03 17:02:32 +0100

    validate

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@
 #   identifier  = "terraform_example_org"
 #   description = "Demo Organization, created through Terraform"
 # }
-resource "harness_platform_project" "project" {  
+resource "harness_platform_project" "project1" {  
     name      = "Terraform Test Project"  
     identifier = "terraform_test_project"  
     org_id    = "default"  
 }
 
-resource "harness_platform_project" "project" {  
+resource "harness_platform_project" "project2" {  
     name      = "Terraform Test Project 2"  
     identifier = "terraform_test_project_2"  
     org_id    = "default"  
